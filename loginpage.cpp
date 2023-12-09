e6623d93957e165d976993f9a89c32a330ec9e60
Sanjib Dahal <sanjibdahal04@gmail.com>
2023-12-09 14:15:49 +0545

    feature: Added message box

--- loginpage.cpp
+++ loginpage.cpp
@@ -26,7 +26,15 @@ void LoginPage::on_loginBtn_clicked()
         admindashboard->showMaximized();
     }
     else {
-        QMessageBox::warning(this,"Dhulikhel Recreational Center","Username or password incorrect!");
+        //QMessageBox::warning(this,"Dhulikhel Recreational Center","Username or password incorrect!");
+        QMessageBox *msgBox = new QMessageBox(this);
+        msgBox->setText("Username or password incorrect!");
+        msgBox->setIcon(QMessageBox::Warning);
+        msgBox->setStandardButtons(QMessageBox::Ok);
+        msgBox->setWindowTitle("Dhulikhel Recreational Center");
+        msgBox->setStyleSheet("QLabel{font-size: 18px; color: #fff; font-weight: 400; font-family: 'Poppins';} QPushButton{"
+                              "color: #fff; font-family: 'Poppins' }");
+        msgBox->exec();
     }
 }
 
@@ -35,20 +43,6 @@ void LoginPage::on_signupBtn_clicked()
 {
     ui->stackedWidget_2->setCurrentIndex(1);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 void LoginPage::on_loginBtn_2_clicked()
